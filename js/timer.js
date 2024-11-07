076849f58614a49a0698836d11f44c07282d74aa
tobi1509 <118448421+tobi1509@users.noreply.github.com>
2024-11-07 15:58:04 +0700

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,6 +1,6 @@
 setTimeout(function () {
   document.body.style.opacity = 0;
   setTimeout(function () {
-    window.location.href = 'html/index2.html'
-  }, 500); // thời gian chờ 0.5 giây
-}, 6000); // thời gian chờ 10 giây
+    window.location.href = 'index2.html'
+  }, 500); 
+}, 6000);
